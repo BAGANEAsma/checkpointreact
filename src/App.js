a15a17f7195635b461a61718badca1465611ac11
BAGANE-Asma <asma.bagane.1993@gmail.com>
2023-03-05 09:54:12 +0100

    CODE1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Asma from './Asma';
+
+function App() {
+  return (
+    <div>
+<Asma></Asma>
+
+    </div>
+  );
+}
+
+export default App;
